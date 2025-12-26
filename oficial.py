81291b2a5ff49c53558cef44a2ebcdd8836450d7
zettaflare1 <marcelomarcosmorelli@gmail.com>
2025-12-26 15:38:46 -0300

    Update oficial.py

--- oficial.py
+++ oficial.py
@@ -2,18 +2,15 @@ import requests
 import sqlite3
 from datetime import date
 
-# 1. Obtener dato
 url = "https://dolarapi.com/v1/dolares/oficial"
 data = requests.get(url).json()
 
 fecha = date.today().isoformat()
 valor = data["venta"]
 
-# 2. Conectar a la base
 conn = sqlite3.connect("datos.db")
 cur = conn.cursor()
 
-# 3. Crear tabla si no existe
 cur.execute("""
 CREATE TABLE IF NOT EXISTS dolar_oficial (
     fecha TEXT PRIMARY KEY,
@@ -21,7 +18,6 @@ CREATE TABLE IF NOT EXISTS dolar_oficial (
 )
 """)
 
-# 4. Insertar dato
 cur.execute("""
 INSERT OR IGNORE INTO dolar_oficial (fecha, valor)
 VALUES (?, ?)
